930b73e19e0dc347fa8f7ef8a02b2714f4dbfd59
munibshah <97636532+munibshah@users.noreply.github.com>
2024-06-23 00:52:28 -0400

    will work now

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World! Welcome", "message": "Welcome to FastAPI!"}
+def read_root():
+    return {"Hello": "World"}
